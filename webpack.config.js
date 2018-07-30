9fa6d6270ad028cdee9702c1faa6957ce14b98d0
Abigail Demsas <ademsas1026@gmail.com>
2018-07-30 15:41:47 -0400

    babel vanquished!!!

--- webpack.config.js
+++ webpack.config.js
@@ -16,11 +16,7 @@ module.exports = {
       {
         test: /\.jsx?$/,
         exclude: /node_modules/,
-        loader: 'babel-loader', 
-        query: {
-          presets: ['env', 'react'],
-          plugins: ['transform-class-properties']
-        }
+        loader: 'babel-loader'
       }
     ]
   }
